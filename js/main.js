f61a2fab87dffe36b67a5447b7692c014479ab50
alampere <pereiracostaala@gmail.com>
2022-04-19 19:04:28 -0300

    second-commit

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
 let ativo = false;
+
+// funcao para selecionar as notas
 const stateActive =  () => {
     const btn = document.querySelectorAll('button');
     const btnSubmit = document.querySelector('#avaliacao');
+    // interar sobre os elementos 
     btn.forEach((element) => {
         ativo = true;
         element.addEventListener('click', (e) => {
             if(ativo === true){
+                // verificar se esta ativo 
                 element.style.background = '#FB7312';
                 const nota = element.value ;
                 const notaAvaliacao = document.querySelector(".msg>p");
                 notaAvaliacao.textContent = ` You selected ${nota} out of 5`;
+                // seta a fariavel para nao selecionar outra nota
                 ativo = false;
             }else {
+                // se estiver ativo alerta para enviar
                 alert ('Por favor Realizar a sua avaliação');
             }  
         }) 
